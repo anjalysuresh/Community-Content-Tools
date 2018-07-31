19486fa2c9c39e3e761242cfd7f95d5413e6858d
anjali <anjalyusresh@cse.iitb.ac.in>
2018-07-31 16:40:39 +0530

    changed h5p.sh

--- h5p_etherpad.sh
+++ h5p_etherpad.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo docker-compose up db
-cd H5p
+sudo docker-compose up -d db
+cd H5P
 sudo docker build -t h5p_image .
 cd
 git clone https://github.com/ether/etherpad-lite 
 cd etherpad-lite/
-sudo cp -r * ../Community-Content-Tools/etherpad-lite/ 
+sudo cp -r * ../Content_Tools/etherpad-lite/ 
 cd
-cd Community-Content-Tools/etherpad-lite/
+cd Content_Tools/etherpad-lite/
 sudo docker build -t etherpadlite .
