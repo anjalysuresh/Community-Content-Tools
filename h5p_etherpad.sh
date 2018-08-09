bc90de9fcd8e221bfcbc2d4f455d4e68e2d78819
anjalysuresh <anjalysuresh@yahoo.co.in>
2018-08-09 13:01:25 +0530

    Update h5p_etherpad.sh

--- h5p_etherpad.sh
+++ h5p_etherpad.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cd Content_Tools
 sudo docker-compose up -d db 
+cd H5P
 sudo sed  's,COLLAB_ROOT=http://172.17.0.1:7000,COLLAB_ROOT=http://10.129.27.30:7000,g' .env.docker
 sudo sed  's,H5P_ROOT=http://172.17.0.1:8000,H5P_ROOT=http://10.129.27.30:8000,g' .env.docker
-cd H5P
+
 sudo docker build -t h5p_image .
 cd ..
 cd ..
